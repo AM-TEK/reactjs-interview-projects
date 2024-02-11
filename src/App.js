6f732b863f87476bd42c230fc45d9a50fa254243
Alejandro Maldonado <am@AM-WEBDEV.attlocal.net>
2024-02-11 14:01:46 -0600

    handle methods for displaying random rgb and hex colors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Accordian from './components/accordian';
 import './App.css';
+import Accordian from './components/accordian';
+import RandomColor from './components/random-color';
 
 function App() {
   
   return (
     <div className="App">
-      <Accordian />
+      {/* <Accordian /> */}
+      <RandomColor />
     </div>
   );
 }
