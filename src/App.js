b97e952279cb59b52c0486b6302f8722717e0c5c
Alejandro Maldonado <am@AM-WEBDEV.attlocal.net>
2024-02-12 11:34:10 -0600

    load more data on page from api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import ImageSlider from './components/image-slider';
+// import ImageSlider from './components/image-slider';
+import LoadMoreData from './components/load-more-data';
 // import Accordian from './components/accordian';
 // import RandomColor from './components/random-color';
 // import StarRating from './components/star-rating';
@@ -11,11 +12,12 @@ function App() {
       {/* <Accordian /> */}
       {/* <RandomColor /> */}
       {/* <StarRating noOfStars={10} /> */}
-      <ImageSlider
+      {/* <ImageSlider
         url={"https://picsum.photos/v2/list"}
         page={"1"}
         limit={"10"}
-      />
+      /> */}
+      <LoadMoreData />
     </div>
   );
 }
