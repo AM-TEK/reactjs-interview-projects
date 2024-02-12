ff72f0e280a267ab614be567581e2e51e63a017c
Alejandro Maldonado <am@AM-WEBDEV.attlocal.net>
2024-02-11 20:44:28 -0600

    fetch data, map through data, handle clicks for slider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
+import ImageSlider from './components/image-slider';
 // import Accordian from './components/accordian';
 // import RandomColor from './components/random-color';
-import StarRating from './components/star-rating';
+// import StarRating from './components/star-rating';
 
 function App() {
   
@@ -9,7 +10,12 @@ function App() {
     <div className="App">
       {/* <Accordian /> */}
       {/* <RandomColor /> */}
-      <StarRating noOfStars={10} />
+      {/* <StarRating noOfStars={10} /> */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </div>
   );
 }
